3da679cd979074afed9434f89ea1263ebfb32f7f
Pablo <pablodfb98@gmail.com>
2025-01-08 20:39:05 -0300

    update(home): updated about section, removed example texts

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import styles from "./About.module.css";
 import aboutImage from "../../../assets/about/aboutImage.png";
-import cursorIcon from "../../../assets/about/cursorIcon.png";
-import serverIcon from "../../../assets/about/serverIcon.png";
-import uiIcon from "../../../assets/about/uiIcon.png";
+import codeIcon from "../../../assets/about/codeIcon.png";
+import libraryIcon from "../../../assets/about/libraryIcon.png";
+import designIcon from "../../../assets/about/designIcon.png";
 
 export const About = () => {
   return (
     <section id="about" className={styles.container}>
       <h2 className={styles.title}>Acerca de</h2>
       <div className={styles.content}>
-            {/* TODO: replace free downloaded image for an actual image created for this site. Don't show it until then  */}
+        {/* TODO: replace free downloaded image for an actual image created for this site. Don't show it until then  */}
         {/* <img
           className={styles.aboutImage}
           src={aboutImage}
@@ -18,32 +18,35 @@ export const About = () => {
         /> */}
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
-            <img src={cursorIcon} alt="Ícono de cursor" />
+            <img src={codeIcon} alt="Ícono de cursor" />
             <div className={styles.aboutItemText}>
               <h3>Front-end developer</h3>
               <p>
-                Soy un desarrollador front-end con experiencia desarrollando
-                sitios responsivos y optimizados
+                Soy un desarrollador front-end con experiencia en múltiples
+                proyectos, desarrollando sitios responsivos y optimizados
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={serverIcon} alt="Ícono de servidor" />
+            <img src={libraryIcon} alt="Ícono de servidor" />
             <div className={styles.aboutItemText}>
-              <h3>Back-end developer</h3>
+              <h3>Librerías</h3>
               <p>
-                Tengo experiencia desarrollando aplicaciones y APIs rápidas y
-                optimizadas
+                Tengo experiencia en el uso de múltiples librerías de JavaScript
+                para desarrollar aplicaciones según los requisitos de cada
+                proyecto
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={uiIcon} alt="Ícono de servidor" />
+            <img src={designIcon} alt="Ícono de servidor" />
             <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
+              <h3>Diseño</h3>
               <p>
-                Tengo experiencia desarrollando aplicaciones y APIs rápidas y
-                optimizadas
+                El diseño gráfico siempre me ha fascinado y ha sido un hobby
+                para mí. Gracias a estos conocimientos, he podido contribuir a
+                los proyectos en los que participo, aportando no solo desde la
+                perspectiva de un desarrollador
               </p>
             </div>
           </li>
